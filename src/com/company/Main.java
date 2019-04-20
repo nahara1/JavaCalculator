5c64ad1f30313c0c54acb918b6f0955a5d487a83
Nahara <44853730+nahara1@users.noreply.github.com>
2019-04-20 12:56:36 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,10 @@
 /*
-Author: Nahara
+Author: Narrara
 Project: Lab 14 - Java Calculator with Unit Testing and Java Docs
 Purpose Details:  
 Course: IST 242 - 001
 Date Developed: 4/15/2019
-Last Date Changed: 4/18/2019
+Last Date Changed: 4/20/2019
 Rev: 1
  */
 
@@ -14,7 +14,7 @@ import java.util.Scanner;
 
 
 /**
- * @author          Nahara 
+ * @author          Narrara 
  * @version         1.0
  * @since           2019-04-15
  */
@@ -23,27 +23,41 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Scanner scnr = new Scanner(System.in);
+
         System.out.println("Enter an integer number: ");
- 
-        int input = scnr.nextInt();    
-	System.out.println("Enter another integer number: ");
+        int input = scnr.nextInt();
+
+        System.out.println("Enter another integer number: ");
         int input2 = scnr.nextInt();
-        System.out.println(input + " + " + input2 + " = " + addition(input, input2));
 
-
+        System.out.println("1 - Addition\n 1 - Subtraction\n 3 - Multiplication\n 4 - Division\n Q - Quit");
+        String input3 = scnr.next();
+        while (!input3.equals("Q")) {
+            switch (input3) {
+                case "1":
+                    System.out.println(input + " + " + input2 + " = " + addition(input, input2));
+                    break;
+                case "2":
+                    System.out.println(input + " + " + input2 + " = " + subtraction(input, input2));
+                    break;
+                case "3":
+                    System.out.println(input + " + " + input2 + " = " + multiplication(input, input2));
+                    break;
+                case "4":
+                    System.out.println(input + " + " + input2 + " = " + division(input, input2));
+                    break;
+            }
+        }
     }
 
-    /**
-     * addition method performs addition
-     * @param num1 an integer number
-     * @param num2 an integer number
-     * @return an integer number
-     */
-
-
- @org.junit.Test
+        /**
+         * addition method performs addition
+         * @param num1 an integer number
+         * @param num2 an integer number
+         * @return an integer number
+         */
 
 
 
@@ -63,9 +77,6 @@ public class Main {
         return num1/num2;
     }
 
-public class Test {
-
-}
 
 
 }
